c6967d92667fea64912401c2522f9e6f2dd337ee
gGonz <zurg.cei@gmail.com>
2013-07-04 14:12:05 -0500

    updated custom cookbook to install redis and gettext

--- cookbooks/custom/recipes/default.rb
+++ cookbooks/custom/recipes/default.rb
@@ -3,26 +3,11 @@ include_recipe "build-essential"
 include_recipe "postgresql::server"
 
 %w{postgresql git-core build-essential tree python ipython
-   python-psycopg2 python-setuptools python-pip}.each do
+   python-psycopg2 python-setuptools python-pip gettext redis-server}.each do
 |pkg|
   package pkg do
     action :install
   end
-end
-
-#
-# Installing redis
-#
-bash "Installing redis..." do
-    code <<-EOH
-    cd /tmp
-    mkdir redis && cd redis
-    wget http://download.redis.io/redis-stable.tar.gz
-    tar xvzf redis-stable.tar.gz
-    cd redis-stable
-    make
-    sudo make install
-    EOH
 end
 
 #
